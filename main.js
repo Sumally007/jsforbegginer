3c4232dec8d33c94ee56f1b77159ab9525d9f372
iamismail <ismailmbarouk@gmail.com>
2024-10-23 20:44:56 +0300

    single and multiple elements

--- main.js
+++ main.js
@@ -16,32 +16,47 @@
 
 // Class
 
-class Person{
-    constructor(firstName, lastName, dob){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dob = new Date(dob);
-    }
+// class Person{
+//     constructor(firstName, lastName, dob){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.dob = new Date(dob);
+//     }
 
 
-    getBirthYear(){
-        return this.dob.getFullYear();
-    }
+//     getBirthYear(){
+//         return this.dob.getFullYear();
+//     }
 
-    getfullName(){
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
-
-
+//     getfullName(){
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
 
 
-// instatiate object
 
-const person1 = new Person('Ismail', 'Said', '5-8-2001');
-const person2 = new Person('gogo', 'mushomary', '3-7-2000');
 
-console.log(person2.getfullName());
+// // instatiate object
 
-console.log(person1);
+// const person1 = new Person('Ismail', 'Said', '5-8-2001');
+// const person2 = new Person('gogo', 'mushomary', '3-7-2000');
 
+// console.log(person2.getfullName());
+
+// console.log(person1);
+
+
+
+// Single element
+// console.log(document.getElementById('my-form'));
+
+// console.log(document.querySelector('.container'));
+
+// Multiple element
+
+// console.log(document.querySelectorAll('.item'));
+
+
+const items = document.querySelectorAll('.item');
+
+items.forEach((item) => console.log(item));
